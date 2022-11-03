0c5c3bd32d008a4d65afbd7fd5a3a41096c148d2
TestRep1 <prn007@gmail.com>
2022-11-03 18:45:24 +0200

    aws deployment

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello NEW111</p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload 123.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn 123
+          Learn React
         </a>
       </header>
     </div>
